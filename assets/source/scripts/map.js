e6ad15d6676a058aadf0306bd9844519969f0002
Vlad <costmc@me.com>
2017-09-12 17:30:23 +0300

    small imp in mobile

--- assets/source/scripts/map.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-$('.embed-responsive')
-	.click(function(){
-			$(this).find('#map').addClass('clicked')})
-	.mouseleave(function(){
-			$(this).find('#map').removeClass('clicked')});
-});
